ba515bc0f1f71e343bf4fae39ee492ee97842dca
Jayveeeee24 <jayveejimenez32@gmail.com>
2024-01-25 15:02:03 +0800

    checkpoint

--- src/screens/bottomTabs/AlbumsTab.tsx
+++ src/screens/bottomTabs/AlbumsTab.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Animated, FlatList, Modal, SafeAreaView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import { Alert, Animated, Dimensions, FlatList, Modal, SafeAreaView, ScrollView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import { Menu, MenuOption, MenuOptions, MenuProvider, MenuTrigger } from "react-native-popup-menu";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { FAB } from "react-native-paper";
@@ -7,32 +7,20 @@ import { Colors } from "../../utils/Config";
 import { IndividualAlbum } from "../../components/IndividualAlbum";
 import { useAlbum } from "../../context/AlbumContext";
 import { usePhoto } from "../../context/PhotoContext";
+import { IndividualSearch } from "../../components/IndividualSearch";
+import SearchBar from "react-native-dynamic-search-bar";
 
 interface AlbumTabProps {
     navigation: any;
     route: any;
 }
 
+const height = Dimensions.get('window').height;
 export const AlbumsTab: React.FC<AlbumTabProps> = ({ navigation, route }) => {
     const { getAllAlbums } = useAlbum();
 
     const [albums, setAlbums] = useState<any>([]);
 
-    const [isBottomSheetVisible, setBottomSheetVisible] = useState(false);
-    const translateY = new Animated.Value(300);
-
-    useEffect(() => {
-        Animated.timing(translateY, {
-            toValue: isBottomSheetVisible ? 0 : 300,
-            duration: 300,
-            useNativeDriver: true,
-        }).start();
-    }, [isBottomSheetVisible]);
-    
-
-    const toggleBottomSheet = () => {
-        setBottomSheetVisible(!isBottomSheetVisible);
-    };
 
     useEffect(() => {
         const getAlbums = async () => {
@@ -48,86 +36,65 @@ export const AlbumsTab: React.FC<AlbumTabProps> = ({ navigation, route }) => {
 
         getAlbums();
     }, [getAllAlbums]);
-
-
-
+    
     return (
         <MenuProvider>
             <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
-                <TouchableWithoutFeedback onPress={() => {
-                    setBottomSheetVisible(false);
-                }}>
-                    <View style={{ flex: 1 }}>
-                        <View style={{ flexDirection: "column", flex: 1 }}>
-                            <View style={{ width: '100%', alignItems: "flex-end" }}>
-                                <Menu>
-                                    <MenuTrigger style={{ marginHorizontal: 10 }}>
-                                        <View style={{ flexDirection: "row", borderColor: 'gray', borderWidth: 1, padding: 5 }}>
-                                            <Text style={{ color: 'black' }}>Sort by: </Text>
-                                            <MaterialIcons name="keyboard-arrow-down" size={20} color="#666" />
+                <View style={{ flex: 1 }}>
+                    <View style={{ flexDirection: "column", flex: 1 }}>
+                        <View style={{ width: '100%', alignItems: "flex-end" }}>
+                            <Menu>
+                                <MenuTrigger style={{ marginHorizontal: 10 }}>
+                                    <View style={{ flexDirection: "row", borderColor: 'gray', borderWidth: 1, padding: 5 }}>
+                                        <Text style={{ color: 'black' }}>Sort by: </Text>
+                                        <MaterialIcons name="keyboard-arrow-down" size={20} color="#666" />
+                                    </View>
+                                </MenuTrigger>
+                                <MenuOptions>
+                                    <MenuOption onSelect={() => Alert.alert('Newest')} >
+                                        <View style={{ flexDirection: "row" }}>
+                                            <MaterialIcons name="check" size={20} color="#666" style={{ display: 'none' }} />
+                                            <Text style={{ color: 'black', marginStart: 10 }}>Newest</Text>
                                         </View>
-                                    </MenuTrigger>
-                                    <MenuOptions>
-                                        <MenuOption onSelect={() => Alert.alert('Newest')} >
-                                            <View style={{ flexDirection: "row" }}>
-                                                <MaterialIcons name="check" size={20} color="#666" style={{ display: 'none' }} />
-                                                <Text style={{ color: 'black', marginStart: 10 }}>Newest</Text>
-                                            </View>
-                                        </MenuOption>
-                                        <MenuOption onSelect={() => Alert.alert(`Oldest`)} >
-                                            <View style={{ flexDirection: "row" }}>
-                                                <MaterialIcons name="check" size={20} color="#666" style={{ display: 'none' }} />
-                                                <Text style={{ color: 'black', marginStart: 10 }}>Oldest</Text>
-                                            </View>
-                                        </MenuOption>
-                                    </MenuOptions>
-                                </Menu>
-                            </View>
+                                    </MenuOption>
+                                    <MenuOption onSelect={() => Alert.alert(`Oldest`)} >
+                                        <View style={{ flexDirection: "row" }}>
+                                            <MaterialIcons name="check" size={20} color="#666" style={{ display: 'none' }} />
+                                            <Text style={{ color: 'black', marginStart: 10 }}>Oldest</Text>
+                                        </View>
+                                    </MenuOption>
+                                </MenuOptions>
+                            </Menu>
+                        </View>
 
-                            <View style={{ marginTop: 10 }}>
-                                <FlatList
-                                    data={albums}
-                                    renderItem={({ item, index }) => (
-                                        <IndividualAlbum albums={albums} index={index} item={item} navigation={navigation} route={route} />
-                                    )}
-                                    keyExtractor={(item) => item.firstPhoto.albumId}
-                                    numColumns={3}
-                                    showsVerticalScrollIndicator={false}
-                                />
-                            </View>
-
-                            <FAB
-                                color="white"
-                                icon='plus'
-                                style={{
-                                    borderRadius: 50,
-                                    position: 'absolute',
-                                    margin: 16,
-                                    right: 0,
-                                    bottom: 0, backgroundColor: Colors.secondaryBrand
-                                }}
-                                onPress={toggleBottomSheet}
+                        <View style={{ marginTop: 10 }}>
+                            <FlatList
+                                data={albums}
+                                renderItem={({ item, index }) => (
+                                    <IndividualAlbum albums={albums} index={index} item={item} navigation={navigation} route={route} />
+                                )}
+                                keyExtractor={(item) => item.firstPhoto.albumId}
+                                numColumns={3}
+                                showsVerticalScrollIndicator={false}
                             />
                         </View>
 
-                        <Animated.View
-                            style={[
-                                {
-                                    transform: [{ translateY: translateY }],
-                                    backgroundColor: 'white',
-                                    borderTopLeftRadius: 20,
-                                    borderTopRightRadius: 20,
-                                    padding: 16,
-                                    position: 'absolute',
-                                    bottom: 0,
-                                    left: 0,
-                                    right: 0,
-                                },
-                            ]}>
-                            <Text>This is the bottom sheet content</Text>
-                        </Animated.View>
+                        <FAB
+                            color="white"
+                            icon='plus'
+                            style={{
+                                borderRadius: 50,
+                                position: 'absolute',
+                                margin: 16,
+                                right: 0,
+                                bottom: 0, backgroundColor: Colors.secondaryBrand
+                            }}
+                            onPress={() => {}}
+                        />
                     </View>
-                </TouchableWithoutFeedback>
+
+                    
+                </View>
             </SafeAreaView>
         </MenuProvider>
     );
